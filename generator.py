dd290216fe543b5c54afa7ba233b0984b3429d8a
Isabelle Phan <isaphan@gmail.com>
2012-03-04 13:10:42 -0800

    add mako template page generator

---
+++ generator.py
@@ -0,0 +1,17 @@
+from mako.template import Template
+import json
+import sys
+
+serie = sys.argv[1]
+datafile = open(sys.argv[2],'r')
+
+json_data = datafile.read()
+data = json.loads(json_data)
+
+mytemplate = Template(filename='templates/spot_tmpl.html')
+
+for mushroom in data['mushrooms']:
+    print mushroom
+    page = open(serie + '/' + mushroom + '.html', 'w')
+    page.write(mytemplate.render(**data['mushrooms'][mushroom]))
+    page.close()
